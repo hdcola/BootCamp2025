6a0e2b7af5f620cb86472507903aaecf7d1da92a
JaDGu <zhujamesf@gmail.com>
2025-05-08 12:19:41 -0400

    guess_number_day2

--- jsts/003.GuessNumber/JamesG/script.js
+++ jsts/003.GuessNumber/JamesG/script.js
@@ -1,20 +1,27 @@
-function greet() {
-	alert('你好，主人！')
-}
+// function greet() {
+// 	alert('你好，主人！')
+// }
 
-function showTime() {
-	const now = new Date()
-	document.getElementById('time').innerText =
-		'现在是：' + now.toLocaleTimeString()
-}
+// function showTime() {
+// 	const now = new Date()
+// 	document.getElementById('time').innerText =
+// 		'现在是：' + now.toLocaleTimeString()
+// }
 
 function restartGame() {
 	random_num = Math.floor(Math.random() * 1000)
-	document.getElementById('result').innerText = random_num
+	document.getElementById('result').innerText = 'please guess'
 }
 
 function checkGuess() {
 	guess0 = -1
 	guess0 = document.getElementById('guess').value
-	document.getElementById('result').innerText = guess0
+	if (guess0 > random_num) {
+		document.getElementById('result').innerText = guess0 + ' too big'
+	} else if (guess0 < random_num) {
+		document.getElementById('result').innerText = guess0 + ' too small'
+	} else {
+		document.getElementById('result').innerText =
+			guess0 + ' you are right!!'
+	}
 }
