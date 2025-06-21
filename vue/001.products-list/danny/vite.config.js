545f1cb57a4eeabdd7c1d09f0faa6fa12e74b8d2
Danny <gh@hdcola.org>
2025-06-21 11:16:09 -0400

    add vue devtools

--- vue/001.products-list/danny/vite.config.js
+++ vue/001.products-list/danny/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './', // 使用相对路径而不是绝对路径
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
 });
