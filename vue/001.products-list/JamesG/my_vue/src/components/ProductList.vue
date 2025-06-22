9473cbab4d49377b8fe270a2269fc4e533ae7c3b
JaDGu <zhujamesf@gmail.com>
2025-06-21 20:17:33 -0400

    vue-初体验

---
+++ vue/001.products-list/JamesG/my_vue/src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ProductItem v-for="aaa in products3" :key="aaa.id" :name="aaa.name" :price="aaa.price" @delete="$emit('delete2',aaa.name)">
+        </ProductItem>
+    </div>
+</template>
+
+<script setup>
+import ProductItem from './ProductItem.vue'
+
+
+const props = defineProps({
+  products3:{
+        type: Object,
+        required: true
+    },
+
+
+    
+})
+
+const emit = defineEmits(['delete2'])
+
+
+
+
+
+</script>
