f72e74a4c7f1a6954525e429f3f0b4d868b0a9cb
ZHENHAO YANG <49468724+Allenyzh@users.noreply.github.com>
2025-11-18 12:45:10 -0500

    -

---
+++ python/002.socket/echo_server.py
@@ -0,0 +1,21 @@
+import socket
+
+PORT = 12345
+HEADER = 1024
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(ADDR)
+    s.listen()
+
+    c, addr = s.accept()
+    with c:
+        print(f"[CONNECTED] Connected by {addr}")
+        while True:
+            data = c.recv(HEADER)
+            if not data:
+                break
+            c.sendall(data)
+
