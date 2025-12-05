5fccdd6531dcd12b738bf1d8c7469be17c94b7a5
JaDGu <zhujamesf@gmail.com>
2025-12-04 23:55:55 -0500

    SB AI对话聊天室完成

--- python/002.socket/JamesG/echo_server.py
+++ python/002.socket/JamesG/echo_server.py
@@ -1,5 +1,10 @@
 import socket
 import threading
+from google import genai
+from google.genai import types
+import os
+# api_key = os.getenv("GOOGLE_API_KEY")
+# print(api_key)
 
 # PORT = 5050
 # server = socket.gethostbyname(socket.gethostname())
@@ -7,14 +12,33 @@ import threading
 # print(socket.gethostname())
 # print(server)
 
+
+
+def call_gemini_api(contentinput: str = "你好") -> str:
+    client = genai.Client()
+
+    # system_instruction = (
+    #     "你是一个少言寡语的得道高僧,回答尽量少于50字,"
+    #     "关键是简洁，而不是准确。"
+    # )
+
+    resp = client.models.generate_content(
+    model="gemini-2.5-flash", contents=f"像高僧一样言简意赅的回答:\n\n{contentinput}")
+    text = resp.candidates[0].content.parts[0].text.strip()
+    return text
+
 def handle_clients(c , addr):
     print(addr, "conneted!")
     while True:
         data = c.recv(1024)
         if not data:
             break
-        c.sendall(data)
+        user_msg = data.decode("utf-8", errors="ignore").strip()
+        if not user_msg:
+                continue
 
+        reply = call_gemini_api(user_msg)
+        c.sendall(reply.encode("utf-8"))
 
 with socket.socket(socket.AF_INET , socket.SOCK_STREAM) as s:
     s.bind(("0.0.0.0", 1234))
@@ -25,11 +49,8 @@ with socket.socket(socket.AF_INET , socket.SOCK_STREAM) as s:
 
         t = threading.Thread(target=handle_clients,args=(c,addr))
         t.start()
-    # with c:
-    #     print(addr, "conneted!")
 
-    #     while True:
-    #         data = c.recv(1024)
-    #         if not data:
-    #             break
-    #         c.sendall(data)
+# print(call_gemini_api("你好"))
+# print("ENV KEY:", repr(os.getenv("GOOGLE_API_KEY")))
+# print("ENV KEY:", repr(os.getenv("GEMINI_API_KEY")))
+# print(os.environ)
